cef52abd7baa3741162edba6dbf06abf81629068
oscar novoa <novthel@live.com>
2022-07-30 14:49:01 -0500

    add button

--- reto/src/components/pure/ComponenteB.jsx
+++ reto/src/components/pure/ComponenteB.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Contacto } from '../../models/contacto_class';
 
 
 const ComponenteB = ({ contacto }) => {
 
+    const [conectado, setConectado] = useState(contacto.conectado);
+
     return (
         <div>
             <h2>Nombre Completo: { contacto.nombre } { contacto.apellido }</h2>
             <h3>Email: { contacto.email }</h3>
-            <h3>Conectado: { contacto.conectado ? 'Contacto En Linea' : 'Contacto No Disponible' } </h3>
+            <h3>Conectado: { conectado ? 'Contacto En Linea' : 'Contacto No Disponible' } </h3>
+            <button onClick={ () => setConectado(!conectado)}>{ conectado ? 'Desconectar' : 'Conectar' }</button>
         </div>
     );
 };
